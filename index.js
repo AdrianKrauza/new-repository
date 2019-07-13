f90fe115e5cb1b67dd8382709bfb66bb821b390b
unknown <adikrauza@gmail.com>
2019-07-13 18:40:24 +0200

    -add private

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const gitPass = require('./gitPass.js');
 const args = process.argv;
 let name = '';
-for (x = 2; x <= args.length - 1; x++) {
+for (x = 2; x <= args.length - 2; x++) {
 	name === '' ? (name += args[x]) : (name += '-' + args[x]);
 }
 (async () => {
@@ -16,6 +16,8 @@ for (x = 2; x <= args.length - 1; x++) {
 	await page2.goto('https://github.com/new');
 	await page2.setViewport({ width: 1500, height: 720 });
 	await page2.type('input#repository_name', name);
+	await page2.waitFor(500);
+	if(process.argv[process.argv.length - 1] === "t") await page2.click('#new_repository > div.js-with-permission-fields > div:nth-child(5) > label > input#repository_visibility_private');
 	await page2.waitFor(500);
 	await page2.click('#new_repository > div.js-with-permission-fields > button');
 	await page2.waitFor(1000);
--- new-repository.bat
+++ new-repository.bat
@@ -1,4 +1,15 @@
 @echo off
-set /p name=" Repository name: "
-node index.js %name%
-pause
+:name
+    cls
+    set /p name=" Repository name: "
+if "%name%" == "" goto :name
+
+:private
+    cls
+    echo Repository name: %name%
+       set /p private=" Repository private(f = false,t = true): "
+if "%private%" NEQ "f" if "%private%" NEQ "t" goto :private
+
+ 
+node index.js %name% %private%
+pause  
