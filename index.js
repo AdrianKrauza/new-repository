d693a471b3d8e0927012b849eaeae000c4289633
unknown <adikrauza@gmail.com>
2019-07-16 21:14:33 +0200

    -add description

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const puppeteer = require('puppeteer');
 const gitPass = require('./gitPass.js');
 const args = process.argv;
 let name = '';
-for (x = 2; x <= args.length - 2; x++) {
+let description = '';
+for (x = 2; x <= args.length; x++) {
+	if(args[x] === "#stop&" ) break
 	name === '' ? (name += args[x]) : (name += '-' + args[x]);
 }
+for (z = x+ 2; z <= args.length - 1; z++) {
+	description === '' ? (description += args[z]) : (description += ' ' + args[z]);
+}
+console.log(description);
+
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
@@ -17,7 +24,9 @@ for (x = 2; x <= args.length - 2; x++) {
 	await page2.setViewport({ width: 1500, height: 720 });
 	await page2.type('input#repository_name', name);
 	await page2.waitFor(500);
-	if(process.argv[process.argv.length - 1] === "t") await page2.click('#new_repository > div.js-with-permission-fields > div:nth-child(5) > label > input#repository_visibility_private');
+	if(process.argv[process.argv.length - 2] === "t") await page2.click('#new_repository > div.js-with-permission-fields > div:nth-child(5) > label > input#repository_visibility_private');
+	await page2.waitFor(500);
+	if(process.argv[process.argv.length - 1] !== "none") await page2.type('#new_repository > div.js-with-permission-fields > dl > dd > input#repository_description',description);
 	await page2.waitFor(500);
 	await page2.click('#new_repository > div.js-with-permission-fields > button');
 	await page2.waitFor(1000);
